36de0c394d0f5e82ec99a34b2ba994bedf3b9d92
Michael Theodorou <13-12742@saeinstitute.edu>
2021-06-11 19:24:34 +0300

    try tospawn

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public GameObject[] spawnPrefab;
+    public GameObject[] spawnClone;
+
+    void Start()
+    {
+        spawnSomething();
+    }
+    void spawnSomething()
+    {
+        spawnClone[0] = Instantiate(spawnPrefab[0], spawnLocations[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+}
