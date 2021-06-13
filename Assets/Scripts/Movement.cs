9e1b61b9ba902f88376c164c60c592807948a1cd
Michael Theodorou <13-12742@saeinstitute.edu>
2021-06-13 17:53:50 +0300

    create move and mouse look for fps player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,10 +9,22 @@ public class Movement : MonoBehaviour
     public float speed = 12f;
     public float gravity = -9.18f;
     Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
     
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
