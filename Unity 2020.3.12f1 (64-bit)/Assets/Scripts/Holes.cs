1f90c5423fdd775eda51bcf72c73f27aa69add17
Michael Theodorou <13-12742@saeinstitute.edu>
2021-06-18 15:51:43 +0300

    add a new version

---
+++ Unity 2020.3.12f1 (64-bit)/Assets/Scripts/Holes.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Holes : MonoBehaviour
+{
+    [SerializeField] 
+     Terrain terrain;
+
+    public bool[,] holes;
+    public int xBase;
+    public int yBase;
+    public int holeWidth;
+    public int holeHeight;
+    public int x;
+    public int y;
+    public int xPos;
+    public int zPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetHoles(int xBase, int yBase, bool[,] holes)
+    {
+        int offSetZ = holeWidth / 2;
+        int offSetX = holeHeight / 2;
+
+        var b = new bool[holeWidth, holeHeight];
+
+        Vector2 originOfCircle = new Vector2(offSetX, offSetZ);
+        for (xBase = 0; x < holeWidth; x++)
+        {
+            for (yBase = 0; y < holeWidth; y++)
+            {
+
+                if (Vector2.Distance(new Vector2(x, y), originOfCircle) <= offSetX)
+                {
+                    b[x, y] = false;
+                }
+                else
+                {
+                    b[x, y] = true;
+                }
+            }
+        }
+
+
+        terrain.terrainData.SetHoles(xPos - offSetX, zPos - offSetZ, b);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity 2020.3.12f1 (64-bit)/Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public float[,] terrainHeightData;
+    public int rowsandcolumns = 0;
+    public float refinement = 0f;
+    public float multiplier = 0f;
+    float perlinNoise = 0f;
+    Terrain terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrainHeightData = new float[rowsandcolumns, rowsandcolumns];
+        terrain = GetComponent<Terrain>();
+
+        for (int i = 0; i<rowsandcolumns; i++)
+        {
+            for (int j = 0; j< rowsandcolumns; j++)
+            {
+                perlinNoise = Mathf.PerlinNoise(i * refinement, j * refinement);
+                terrainHeightData[i, j] = perlinNoise * multiplier;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, terrainHeightData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
